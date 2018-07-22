78beb1869d3c2c90191ebce011155a03ac2fba21
Nujian Den Mark Meralpis <meralpisdenmark@gmail.com>
2018-07-22 15:33:12 +0800

    fix cop offense

--- lib/api_queries.rb
+++ lib/api_queries.rb
@@ -9,11 +9,11 @@ module ApiQueries
     def api_q(opts={})
       # last updated at q
       if opts[:q] == 'last_updated_at'
-        if count > 0
-          return { last_updated_at: order('updated_at DESC').limit(1).first.updated_at.strftime('%Y-%m-%dT%H:%M:%SZ') }
-        else
-          return { last_updated_at: nil }
-        end
+        return { last_updated_at: (begin
+                                     order('updated_at DESC').limit(1).first.updated_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                                   rescue StandardError
+                                     nil
+                                   end) }
       end
 
       # condition hash
@@ -22,19 +22,15 @@ module ApiQueries
       # AFTER: updated_at > given_date
       if opts[:after].present?
         conditions = ['updated_at > ?', fdate(opts[:after])]
-
       # BEFORE: updated_at < given_date
       elsif opts[:before].present?
         conditions = ['updated_at < ?', fdate(opts[:before])]
-
       # FROM & TO: between "from date" to "to date"
       elsif opts[:from].present? && opts[:to].present?
         conditions[:updated_at] = (fdate(opts[:from])..fdate(opts[:to]))
-
       # FROM: updated_at >= given_date
       elsif opts[:from].present?
         conditions = ['updated_at >= ?', fdate(opts[:from])]
-
       # TO: updated_at <= given_date
       elsif opts[:to].present?
         conditions = ['updated_at <= ?', fdate(opts[:to])]
@@ -42,14 +38,11 @@ module ApiQueries
 
       # get by status
       conditions[:status] = 'active' if opts[:active_only].to_s == '1' && conditions.is_a?(Hash)
-
       # return hash
       records = where(conditions)
-
       # get by status
       records = records.where(status: 'active') if opts[:active_only].to_s == '1' && conditions.is_a?(Array)
 
-      # count
       if opts[:q] == 'count'
         { count: records.count }
       else
@@ -63,6 +56,5 @@ module ApiQueries
       Date.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ') # YYYY-MM-DDTHH:MM:SSZ
     end
   end
-
   extend ClassMethods
 end
